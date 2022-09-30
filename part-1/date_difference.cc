1a29a069a9880df730854a7b6e102e996ea9a78e
Christian Bonilla <alton77@csu.fullerton.edu>
2022-09-30 08:15:59 -0700

    just waiting to get a parther

--- part-1/date_difference.cc
+++ part-1/date_difference.cc
@@ -5,9 +5,9 @@
 // @alton7759
 //
 // Lab 04-01
-// Partners: @
+// Partners: @taco
 //
-//Program to calculate the number of days between two Gregorian dates.
+// Program to calculate the number of days between two Gregorian dates.
 //
 
 #include <iostream>
@@ -17,37 +17,35 @@ int main(int argc, char const *argv[]) {
   cout << "Let's find the number of days between two dates...\n";
   int start_month{0};
   cout << "Enter a start month: ";
-  cin >> start_month; 
+  cin >> start_month;
   int start_day{0};
   cout << "Enter a start day: ";
   cin >> start_day;
   int start_year{0};
   cout << "Enter a start year: ";
   cin >> start_year;
-  int start_number_of_days_since_epoch{start_day - 32075 + 1461
-    * (start_year + 4800 + (start_month - 14) / 12) / 4
-    + 367 * (start_month - 2 - (start_month - 14) / 12 * 12) / 12 - 3
-    * ((start_year + 4900 + (start_month - 14) / 12) / 100) / 4};
-
-
-  cout << "\n";
+  int start_number_of_days_since_epoch{
+      start_day - 32075 +
+      1461 * (start_year + 4800 + (start_month - 14) / 12) / 4 +
+      367 * (start_month - 2 - (start_month - 14) / 12 * 12) / 12 -
+      3 * ((start_year + 4900 + (start_month - 14) / 12) / 100) / 4};
   int end_month{0};
-  cout << "Enter a end month: ";
+  cout << "Enter an end month: ";
   cin >> end_month;
   int end_day{0};
-  cout << "Enter a end day: ";
+  cout << "Enter an end day: ";
   cin >> end_day;
   int end_year{0};
-  cout << "Enter a end year: ";
+  cout << "Enter an end year: ";
   cin >> end_year;
-  int end_number_of_days_since_epoch{end_day - 32075 + 1461
-    * (end_year + 4800 + (end_month - 14) / 12) / 4
-    + 367 * (end_month - 2 - (end_month - 14) / 12 * 12) / 12 - 3
-    * ((end_year + 4900 + (end_month - 14) / 12) / 100) / 4};
-
-  int number_days{end_number_of_days_since_epoch - start_number_of_days_since_epoch};
-  cout << "The number of days between " << start_month << "/" << start_day << "/" << start_year
-       << " and " << end_month << "/" << end_day << "/" << end_year << " is " << number_days 
-       << " days\n";
+  int end_number_of_days_since_epoch{
+      end_day - 32075 + 1461 * (end_year + 4800 + (end_month - 14) / 12) / 4 +
+      367 * (end_month - 2 - (end_month - 14) / 12 * 12) / 12 -
+      3 * ((end_year + 4900 + (end_month - 14) / 12) / 100) / 4};
+  int number_days{end_number_of_days_since_epoch -
+                  start_number_of_days_since_epoch};
+  cout << "The number of days between " << start_month << "/" << start_day
+       << "/" << start_year << " and " << end_month << "/" << end_day << "/"
+       << end_year << " is " << number_days << " days\n";
   return 0;
 }
